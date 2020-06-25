cd9b9198611110a7b7b05690fdba801e482674b1
Nida Munir <nida.munir@emumba.com>
2020-06-25 23:43:12 +0500

    Extend List Item Component to Accept an ImageComponent

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -9,6 +9,7 @@ import colors from "../config/colors";
 
 export const ListItem = ({
   image,
+  ImageComponent,
   title,
   subTitle,
   onPress,
@@ -22,10 +23,11 @@ export const ListItem = ({
     >
       <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
         <View style={styles.container}>
-          <Image source={image} style={styles.image} />
-          <View>
+          {ImageComponent}
+          {image && <Image source={image} style={styles.image} />}
+          <View style={styles.detailsContainer}>
             <AppText style={styles.title}>{title}</AppText>
-            <AppText style={styles.subTitle}>{subTitle}</AppText>
+            {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
           </View>
         </View>
       </TouchableHighlight>
@@ -42,10 +44,13 @@ const styles = StyleSheet.create({
     width: 70,
     height: 70,
     borderRadius: 35,
-    marginRight: 10,
   },
   title: {
     fontWeight: "500",
+  },
+  detailsContainer: {
+    marginLeft: 10,
+    justifyContent: "center",
   },
   subTitle: {
     color: colors.medium,
