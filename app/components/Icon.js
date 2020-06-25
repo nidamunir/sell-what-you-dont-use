ed05ffa829ecb44727bfa5a890b32da8dffe7e4c
Nida Munir <nida.munir@emumba.com>
2020-06-25 23:42:46 +0500

    Add Reusable Icon Component

---
+++ app/components/Icon.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { MaterialCommunityIcons } from "react-native-vector-icons";
+import { View } from "react-native";
+
+export const Icon = ({
+  name,
+  size = 40,
+  backgroundColor = "#000",
+  iconColor = "#fff",
+}) => {
+  return (
+    <View
+      style={{
+        width: size,
+        height: size,
+        borderRadius: size * 0.5,
+        backgroundColor,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <MaterialCommunityIcons name={name} size={size * 0.5} color={iconColor} />
+    </View>
+  );
+};
