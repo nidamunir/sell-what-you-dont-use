b3b30fcc9e9a0eb93ad31be190b1ef2fdf185b7b
Nida Munir <nida.munir@emumba.com>
2020-06-26 15:59:39 +0500

    Change Screen Name

--- App.js
+++ App.js
@@ -9,9 +9,12 @@ import { AppButton } from "./app/components/AppButton";
 import { AppCard } from "./app/components/AppCard";
 import { ListingDetailsScreen } from "./app/screens/ListingDetailsScreen";
 import { MessagesScreen } from "./app/screens/MessagesScreen";
+import { Screen } from "./app/components/Screen";
+import { Icon } from "./app/components/Icon";
+import { ListingsScreen } from "./app/screens/ListingsScreen";
 
 export default function App() {
-  return <MessagesScreen />;
+  return <ListingsScreen />;
 }
 
 const styles = StyleSheet.create({
